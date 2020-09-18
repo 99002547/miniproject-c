00980c2739a6ad755b476e991379dc09c25fa93e
99002547 <71488219+99002547@users.noreply.github.com>
2020-09-19 05:25:06 +0530

    Create calc.h

---
+++ calculator/3_Implementation/inc/calc.h
@@ -0,0 +1,23 @@
+#ifndef __CALC_H__
+#define __CALC_H__
+
+int add(int num1, int num2);
+int subtract(int num1, int num2);
+int multiply(int num1, int num2);
+int divide(int num1, int num2);
+int mod(int num1,int num2);
+int fact(int num);
+float sine(int num);
+float cosine(int num);
+float tanget(int num);
+float exponential(int num);
+int power(int m,int n);
+float square_root(float num);
+float cube_root(float num);
+int square(int num);
+int cube(int num);
+float radians( float degree);
+float degrees(float radian);
+float logorithm(float x);
+float lane(float x);
+#endif
