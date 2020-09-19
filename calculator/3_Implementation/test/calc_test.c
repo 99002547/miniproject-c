6efb52df9148396ddae72d67989d2c9169d70526
99002547 <71488219+99002547@users.noreply.github.com>
2020-09-19 13:52:14 +0530

    Update calc_test.c

--- calculator/3_Implementation/test/calc_test.c
+++ calculator/3_Implementation/test/calc_test.c
@@ -10,6 +10,12 @@ void test_add(void);
 void test_subtract(void);
 void test_multiply(void);
 void test_divide(void);
+void test_fact(void);
+void test_mod(void);
+void test_power(void);
+void test_sine(void);
+void test_cosine(void);
+void test_tanget(void);
 
 /* Start of the application test */
 int main() {
@@ -26,6 +32,12 @@ int main() {
   CU_add_test(suite, "subtract", test_subtract);
   CU_add_test(suite, "multiply", test_multiply);
   CU_add_test(suite, "divide", test_divide);
+   CU_add_test(suite, "fact", test_fact);
+  CU_add_test(suite, "mod", test_mod);
+  CU_add_test(suite, "power", test_power);
+  CU_add_test(suite, "sine", test_sine);
+  CU_add_test(suite, "cosine", test_cosine);
+   CU_add_test(suite, "tanget", test_tanget);
 
 
 /* Note: Do not edit START*/
@@ -48,7 +60,30 @@ void test_add(void) {
   /* Dummy fail*/
   CU_ASSERT(1500 == add(750, 7500));
 }
-
+void test_fact(void)
+{
+  CU_ASSERT(24==fact(4));
+}
+void test_mod(void)
+{
+  CU_ASSERT(0==mod(4,2));
+}
+void test_sine(void)
+{
+  CU_ASSERT(1==sine(90));
+}
+void test_cosine(void)
+{
+  CU_ASSERT(1==cosine(0));
+}
+void test_tanget(void)
+{
+  CU_ASSERT(1==tanget(45));
+}
+void test_power(void)
+{
+  CU_ASSERT(16==power(4,2));
+}
 void test_subtract(void) {
   CU_ASSERT(-3 == subtract(0, 3));
   
